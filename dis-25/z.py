84ea65958ce70621983153e76a2d3ca9e659c618
Newton Ni <nwtnni@gmail.com>
2018-11-26 14:19:06 -0500

    Add alternate implementations to python version

--- dis-25/z.py
+++ dis-25/z.py
@@ -1,3 +1,11 @@
+def self_pre_fact(f):
+    return lambda n: 1 if n == 0 else n * f(f)(n - 1)
+
+
+def self_fact(n):
+    return self_pre_fact(self_pre_fact)(n)
+
+
 def z(f):
     return (lambda x: f(lambda y: x(x)(y)))(lambda x: f(lambda y: x(x)(y)))
 
@@ -6,8 +14,16 @@ def pre_fact(f):
     return lambda n: 1 if n == 0 else n * f(n - 1)
 
 
-def fact():
-    return z(pre_fact)
+def fact(n):
+    return z(pre_fact)(n)
 
 
-print(fact()(3))
+def pre_collatz(f):
+    return lambda n: 0 if n == 1 else 1 + f(n // 2) if n % 2 == 0 else 1 + f(3 * n + 1)
+
+
+def collatz(n):
+    return z(pre_collatz)(n)
+
+
+print(collatz(12))
