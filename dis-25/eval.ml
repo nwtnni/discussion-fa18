6a536248e047bd9d2f6dd22852c074e2730c4b15
Newton Ni <nwtnni@gmail.com>
2018-11-26 04:43:02 -0500

    Pull out functions as top-level declarations

--- dis-25/eval.ml
+++ dis-25/eval.ml
@@ -11,36 +11,40 @@ let rec free = function
 | Abs (x, e) -> S.remove x (free e)
 | App (e1, e2) -> S.union (free e1) (free e2)
 
-let rec sub e v x =
-  let rec rename = function
-  | Var y -> Var y
-  | App (e1, e2) -> App (rename e1, rename e2)
-  | Abs (y, e) ->
-    let e' = rename e in
-    let y' = ref y in
-    let fvs = free e' in
-    let incr =
-      let counter = ref None in
-      fun () -> match !counter with
-      | None   -> counter := Some 0;
-      | Some n -> counter := Some (n + 1); y' := (y ^ (string_of_int n))
-    in
-    while !y' = x || S.mem !y' fvs do
-      incr ()
-    done;
-    Abs (!y', e')
-  in
+let incr y =
+  let counter = ref None in
+  fun () -> match !counter with
+  | None   ->
+    counter := Some 0;
+    y
+  | Some n ->
+    counter := Some (n + 1);
+    y ^ (string_of_int n)
 
+let rec rename x = function
+| Var y -> Var y
+| App (e1, e2) ->
+  App (rename x e1, rename x e2)
+| Abs (y, e) ->
+  let e' = rename x e in
+  let fvs = free e' in
+  let y' = ref y in
+  let next = incr y in
+  while !y' = x || S.mem !y' fvs do
+    y' := next ()
+  done;
+  Abs (!y', e')
+
+let sub e v x =
   let rec sub' v x = function
   | Var y when x = y -> v
   | Var y -> Var y
   | Abs (y, e) -> Abs (y, sub' v x e)
   | App (e1, e2) -> App (sub' v x e1, sub' v x e2)
   in
+  rename x e |> sub' v x
 
-  rename e |> sub' v x
-
-and step_by_value = function
+let rec step_by_value = function
 | App (Abs (x, e1), e2) when Ast.is_value e2 ->
   Some(sub e1 e2 x)
 | App (e1, e2) when Ast.is_value e1 ->
@@ -51,7 +55,7 @@ and step_by_value = function
   Some (App (e1', e2))
 | _ -> None
 
-and step_by_name = function
+let rec step_by_name = function
 | App (Abs (x, e1), e2) ->
   Some (sub e1 e2 x)
 | App (e1, e2) ->
